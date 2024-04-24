a527c5e2e2df15d9a72b56a5e8e072da26fec1fd
chaikivskii <164160447+chaikivskii@users.noreply.github.com>
2024-04-24 12:30:49 +0300

    Add files via upload

---
+++ laba 4/4,2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class Income
+{
+    public double Amount { get; set; }
+
+    public abstract double CalculateTax();
+}
+
+public class SalaryIncome : Income
+{
+    public override double CalculateTax()
+    {
+        return Amount * 0.2; // 20% tax
+    }
+}
+
+public class GiftIncome : Income
+{
+    public override double CalculateTax()
+    {
+        return Amount * 0.15; // 15% tax
+    }
+}
+
+public class Person
+{
+    public List<Income> Incomes { get; set; }
+
+    public Person()
+    {
+        Incomes = new List<Income>();
+    }
+
+    public void AddIncome(Income income)
+    {
+        Incomes.Add(income);
+    }
+
+    public double CalculateTotalTax()
+    {
+        return Incomes.Sum(i => i.CalculateTax());
+    }
+
+    public void SortTaxes()
+    {
+        Incomes = Incomes.OrderBy(i => i.CalculateTax()).ToList();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        person.AddIncome(new SalaryIncome { Amount = 50000 });
+        person.AddIncome(new GiftIncome { Amount = 10000 });
+
+        Console.WriteLine("Total tax: " + person.CalculateTotalTax());
+
+        person.SortTaxes();
+
+        foreach (var income in person.Incomes)
+        {
+            Console.WriteLine("Tax for income of " + income.Amount + ": " + income.CalculateTax());
+        }
+    }
+}
---
+++ laba 4/laba 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            Console.WriteLine($"Вид дерева: {tree.Species}");
+            Console.WriteLine();
+            tree.Leaf.Blossom();
+            Console.WriteLine();
+            tree.Leaf.Fall();
+            Console.WriteLine();
+            tree.Branch.CoverWithHoarfrost();
+            Console.WriteLine();
+            tree.Leaf.TurnYellow();
+        }
+    }
+    public class Tree
+    {
+        public string Species { get; } = "Яблуння";
+
+        public Leaf Leaf { get; } = new Leaf();
+
+        public Branch Branch { get; } = new Branch();
+    }
+    public class Leaf
+    {
+        public void Blossom()
+        {
+            Console.WriteLine("Лист зацвітає.");
+        }
+
+        public void Fall()
+        {
+            Console.WriteLine("Лист опадає.");
+        }
+
+        public void TurnYellow()
+        {
+            Console.WriteLine("Лист пожовтів.");
+        }
+    }
+    public class Branch
+    {
+        public void CoverWithHoarfrost()
+        {
+            Console.WriteLine("Гілка покривається інеєм.");
+        }
+    }
+}
